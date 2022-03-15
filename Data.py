d149f058fddc31ec246fbfdb6b5e3e8b1c3814de
Chloe Decker <69217587+TheOnlyMrLucifer@users.noreply.github.com>
2022-03-15 11:04:53 +0100

    V1
    V1

---
+++ Data.py
@@ -0,0 +1,64 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Hey {}
+
+Welcome to {}
+
+I can download profile pictures, videos, images and reels from instagram along with post caption.
+You can also authorize me to download private posts.
+
+Use below buttons to learn more.
+
+By ✨ WikiLeaks ✨ @TheOnlyMrLucifer
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("✨ More Bots And Updates ✨", url="https://t.me/WikiLeaksBots")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+        ],
+        [InlineKeyboardButton("✨ More Amazing bots ✨", url="https://t.me/WikiLeaksBots")],
+    ]
+
+    # Help Message
+    HELP = """
+1) **Images, Videos and Reels**
+Send the link here to get the post contents including caption.
+
+2) **Profile Pictures**
+Use the command `/profile_pic` or `/dp` along with instagram username to get its profile picture.
+Example : `/dp StarkProgrammer`
+
+3) **Private Posts**
+Authorize the bot to download private posts. Use /auth
+
+**Note** : Stories and IGTV are not supported.
+
+Use /auth to authorize and /unauth to unauthorize.
+"""
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+
+A telegram bot to download instagram content by @TheOnlyMrLucifer
+
+Source Code : [Click Here](https://github.com/TheOnlyMrLucifer/WikiInsta)
+
+Framework : [Pyrogram](docs.pyrogram.org)
+
+Language : [Python](www.python.org)
+
+Developer : @TheOnlyMrLucifer
+    """
---
+++ README.md
@@ -0,0 +1,43 @@
+
+
+## Usage
+
+### Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/TheOnlyMrLucifer/WikiInsta)
+
+1. Tap on above button and fill `API_ID`, `API_HASH`, `BOT_TOKEN` (and `MUST_JOIN`).
+2. Then tap "Deploy App" below it. Wait till deploying is complete (will take atmost 2 minutes).
+3. After deploying is complete, tap on "Manage App"
+4. Check the logs to see if your bot is ready!
+
+## Environment Variables
+
+#### Mandatory Vars
+
+- `API_ID` - Get this from [my.telegram.org](https://my.telegram.org/auth)
+- `API_HASH` - Get this from [my.telegram.org](https://my.telegram.org/auth)
+- `BOT_TOKEN` - Get this from [@BotFather](https://t.me/BotFather)
+- `DATABASE_URL` - Will be automatically added by Heroku.
+- `MUST_JOIN` - Username/ID of your telegram channel/group.
+
+## Functions
+
+> More features soon if suggested by you :)
+
+1) Download images and videos.
+2) Download reels.
+3) Download profile pictures.
+4) Download multiple media of a single post.
+5) Get the original caption along with media.
+6) Authorize to download private posts.
+7) Unauthorize when needed.
+
+## To-Do
+
+> That's on you mainly...
+Stories and Profiles
+
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+
